27a4ada7575f8212eab5053847071f0e4e9f2a58
Samyak Sarnayak <samyak@e6data.com>
2026-02-25 18:54:38 +0530

    update rustaceanvim and use codesettings for .vscode/settings.json

--- lua/plugins/rust_plugins.lua
+++ lua/plugins/rust_plugins.lua
@@ -1,7 +1,19 @@
 return {
     {
         "mrcjkb/rustaceanvim",
-        version = "^7", -- Recommended
+        version = "^8", -- Recommended
         lazy = false, -- This plugin is already lazy
+    },
+    {
+        "mrjones2014/codesettings.nvim",
+        opts = {},
+        init = function()
+            vim.lsp.config("*", {
+                before_init = function(_, config)
+                    local codesettings = require("codesettings")
+                    codesettings.with_local_settings(config.name, config)
+                end,
+            })
+        end,
     },
 }
